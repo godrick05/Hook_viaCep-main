50da687d2e19bd6137a898ad38e9b34a9d7d3b37
godrick05 <76274328+godrick05@users.noreply.github.com>
2021-07-21 12:19:42 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App(props) {
+
+  const [cep, setCep] = useState({});
+
+  function handleTextChange(e) {
+    e.preventDefault();
+    fetch(`https://viacep.com.br/ws/${e.target.value}/json/`)
+    .then(res => res.json())
+    .then(res => setCep(res))
+  }
+
+  return (
+    <div className="App">
+      <strong>Olá, {props.name}</strong>
+      <div>
+        digite o CEP: 
+        <input
+          type="text"
+          onChange={handleTextChange}
+        >
+        </input>
+        {Object.entries(cep).map((values, key) => (
+          <h2 key={key}><strong>{values[0]}:</strong> {values[1]}</h2>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
